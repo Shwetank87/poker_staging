507019fa457b83b39175f4ca3d2501f2721f0870
Shwetank Raghuvanshi <shwetank.raghuvanshi@gmail.com>
2014-02-12 01:33:07 -0500

    adding tests 2

--- tests/org/poker/client/PokerLogicTest.java
+++ tests/org/poker/client/PokerLogicTest.java
@@ -45,6 +45,8 @@ public class PokerLogicTest {
       ImmutableList.<Map<String, Object>>of(p0_info, p1_info, p2_info, p3_info);
 
   private final ImmutableMap<String, Object> emptyState = ImmutableMap.<String, Object>of();
+  private final ImmutableMap<String, Object> nonEmptyState = ImmutableMap.<String, Object>of(
+      "K", "V");
   
   
   private VerifyMove move(int lastMovePlayerId, Map<String, Object> lastState,
@@ -64,7 +66,6 @@ public class PokerLogicTest {
     List<Operation> initialOperations = getInitialOperations(
         new int[]{p0_id, p1_id},
         new int[]{2000, 2000});
-
     VerifyMove verifyMove = move(p0_id, emptyState, initialOperations, playersInfo_2_players);
     VerifyMoveDone verifyMoveDone = pokerLogic.verify(verifyMove);
     assertEquals(0, verifyMoveDone.getHackerPlayerId());
@@ -75,10 +76,41 @@ public class PokerLogicTest {
     List<Operation> initialOperations = getInitialOperations(
         new int[]{p0_id, p1_id, p2_id, p3_id},
         new int[]{2000, 2000, 2000, 2000});
-
     VerifyMove verifyMove = move(p0_id, emptyState, initialOperations, playersInfo_4_players);
-    VerifyMoveDone verifyMoveDone = pokerLogic.verify(verifyMove);
-    assertEquals(0, verifyMoveDone.getHackerPlayerId());
+    assertMoveOk(verifyMove);
   }
+  
+  @Test
+  public void testInitialMoveWrongPlayer() {
+    List<Operation> initialOperations = getInitialOperations(
+        new int[]{p0_id, p1_id, p2_id, p3_id},
+        new int[]{2000, 2000, 2000, 2000});
+    VerifyMove verifyMove = move(p1_id, emptyState, initialOperations, playersInfo_4_players);
+    assertHacker(verifyMove);
+  }
+  
+  @Test
+  public void testInitialMoveFromNonEmptyState() {
+    List<Operation> initialOperations = getInitialOperations(
+        new int[]{p0_id, p1_id, p2_id, p3_id},
+        new int[]{2000, 2000, 2000, 2000});
+    VerifyMove verifyMove = move(p0_id, nonEmptyState, initialOperations, playersInfo_4_players);
+    assertMoveOk(verifyMove);
+  }
+  
+  /*
+   * Utility methods copied from
+   * https://github.com/yoav-zibin/cheat-game/blob/master/eclipse/tests/org/cheat/client/CheatLogicTest.java
+   */
+  
+  private void assertMoveOk(VerifyMove verifyMove) {
+    pokerLogic.verify(verifyMove);
+  }
+
+  private void assertHacker(VerifyMove verifyMove) {
+    VerifyMoveDone verifyDone = pokerLogic.verify(verifyMove);
+    assertEquals(verifyMove.getLastMovePlayerId(), verifyDone.getHackerPlayerId());
+  }
+
 
 }
